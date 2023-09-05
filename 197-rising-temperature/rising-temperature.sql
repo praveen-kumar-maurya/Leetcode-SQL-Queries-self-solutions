5a920c9530930687acfb53df5661f7ca4059918f
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-05 16:19:46 +0530

    Time: 920 ms (64.76%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,29 +1,3 @@
-# Write your MySQL query statement below
-/*
-SELECT X.id 
-FROM WEATHER X,WEATHER Y 
-WHERE DATEDIFF(X.recordDate,Y.recordDate) = 1 AND X.temperature > Y.temperature;
-*/
-
-/* 12 out of 14 cases are only working, don't know why */
-/*
-WITH CTE AS
-(
-    SELECT id, DATE_FORMAT(recordDate, '%y-%m-%d') AS recordDate, temperature 
-    FROM Weather 
-),
-
-t1 AS
-(
-    SELECT *
-    ,LAG(temperature) OVER(ORDER BY recordDate ASC) as prev
-    FROM CTE
-)
-
-SELECT id
-FROM t1
-WHERE temperature - prev > 0
-*/
 WITH cte AS
 (
   SELECT *
@@ -35,6 +9,3 @@ WITH cte AS
 SELECT id
 FROM cte
 WHERE temperature > prev and DATEDIFF(recordDate,prev_date)= 1 ;
-
-
-
