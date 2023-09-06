43fd56c42e989d95475f6d9c58d58076379ecafa
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-06 18:35:39 +0530

    Time: 411 ms (89.05%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+# using window function
+/*
 WITH cte as
 (SELECT *
 , LEAD(timestamp) OVER(PARTITION BY machine_id, process_id ORDER BY timestamp) as end
@@ -9,3 +11,13 @@ SELECT machine_id
 ,ROUND(SUM(COALESCE((end-timestamp),0))/ COUNT(DISTINCT(process_id)),3) AS processing_time
 FROM cte
 GROUP BY machine_id;
+*/
+
+
+SELECT a1.machine_id AS machine_id, ROUND(avg(a2.timestamp - a1.timestamp),3) AS processing_time
+FROM activity a1
+JOIN activity a2
+ON a1.machine_id = a2.machine_id
+and a1.process_id = a2.process_id
+and a1.activity_type = 'start' and a2.activity_type = 'end'
+GROUP BY a1.machine_id;
