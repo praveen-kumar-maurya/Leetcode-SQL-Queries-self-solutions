6fc29c673f86529fab91b3f58ae30a3c5aae4665
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-06 18:16:57 +0530

    Time: 825 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -4,12 +4,6 @@ WITH cte as
 , LEAD(timestamp) OVER(PARTITION BY machine_id, process_id ORDER BY timestamp) as end
 FROM activity
 )
-/*
-SELECT 
-timestamp, end,COALESCE((end-timestamp),0)
-FROM cte
-GROUP BY machine_id;
-*/
 
 SELECT machine_id
 ,ROUND(SUM(COALESCE((end-timestamp),0))/ COUNT(DISTINCT(process_id)),3) AS processing_time
