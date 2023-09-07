473797d894db0176434267ea2d12fca920ba2bfd
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-07 16:20:46 +0530

    Time: 2157 ms (9.31%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+/*
+# Using CTE
 WITH cte AS
 (
   SELECT s.user_id as user_id
@@ -13,3 +15,12 @@ GROUP BY s.user_id
 SELECT user_id, COALESCE(ROUND((total_confirmed/total_requests),2),0
  ) AS confirmation_rate
 FROM cte;
+*/
+
+# Simple
+SELECT s.user_id as user_id
+, COALESCE(ROUND((SUM(CASE WHEN action ='confirmed' THEN 1 ELSE 0 END ) / COUNT(c.user_id)),2),0) AS confirmation_rate
+FROM signups AS s
+LEFT JOIN confirmations AS c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
