e6e4f5eb7673d45fc1ce728f486b8ca5420be254
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-08 17:55:37 +0530

    Time: 1447 ms (51.41%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
+/*
 SELECT q.query_name, ROUND(AVG(q.rating/q.position),2) AS quality, COALESCE(ROUND(a.rating_count*100/COUNT(q.rating) ,2),0) AS poor_query_percentage
 FROM queries AS q
 LEFT JOIN
 (SELECT query_name,COUNT(rating) AS rating_count FROM queries WHERE rating<3 GROUP BY query_name) AS a
 ON q.query_name = a.query_name
 GROUP BY q.query_name;
+*/
 
+# Simpler way
+SELECT query_name, ROUND(AVG(rating/position),2) AS quality, COALESCE(ROUND(SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END)*100/COUNT(*) ,2),0) AS poor_query_percentage
+FROM queries
+GROUP BY query_name;
