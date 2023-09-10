b7b0d29450faa80705255f31305b343054f14162
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-10 15:52:59 +0530

    Time: 2078 ms (49.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+WITH cte AS
+(
+SELECT *
+, DENSE_RANK() OVER(PARTITION BY product_id ORDER BY year) as rn
+FROM sales s
+)
+
+SELECT c.product_id AS product_id, c.year AS first_year, c.quantity AS quantity,c.price AS price
+FROM cte c
+LEFT JOIN product p
+ON c.product_id = p.product_id
+WHERE c.rn = 1
+#GROUP BY product_id,first_year;
