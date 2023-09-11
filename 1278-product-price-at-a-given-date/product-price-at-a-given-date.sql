954e476be9a424a62336f8fd5970cbe1613eb384
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-11 17:41:17 +0530

    Time: 1142 ms (40.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH cte AS
+(
+SELECT *
+, MAX(change_date) OVER(PARTITION BY product_id) AS max_change_date
+FROM products
+WHERE change_date <= "2019-08-16"
+)
+
+SELECT product_id, new_price AS price
+FROM cte
+WHERE max_change_date = change_date
+
+UNION
+
+SELECT p.product_id AS product_id, CAST('10' as SIGNED) AS price
+FROM cte c
+RIGHT JOIN products p
+ON c.product_id = p.product_id
+WHERE c.product_id IS NULL;
