3755db502f53e5acb9067557d3e554713b511592
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-12 14:49:36 +0530

    Time: 3286 ms (17.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH cte AS
+(
+SELECT *, SUM(weight) OVER ( order by turn) AS total_weight
+FROM queue
+)
+
+SELECT person_name
+FROM queue
+WHERE person_id NOT IN 
+(
+SELECT COALESCE(person_id, 0)
+FROM cte
+WHERE total_weight > 1000
+) 
+ORDER BY turn DESC
+LIMIT 1;
