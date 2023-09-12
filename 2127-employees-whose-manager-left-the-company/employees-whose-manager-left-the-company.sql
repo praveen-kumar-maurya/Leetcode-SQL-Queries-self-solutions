7693253bb3ae75666479b67ccfeb25650506ef80
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-12 15:59:41 +0530

    Time: 650 ms (89.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+WITH cte as
+(
+SELECT *
+FROM employees
+WHERE salary < 30000
+)
+
+SELECT employee_id
+FROM cte 
+WHERE manager_id IS NOT NULL AND manager_id  NOT IN (SELECT DISTINCT employee_id FROM employees)
+ORDER BY employee_id;
