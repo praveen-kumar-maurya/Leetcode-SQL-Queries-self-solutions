03bc85c08c6023158e50f16fdcc217b49ebec7a0
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-13 21:50:27 +0530

    Time: 428 ms (89.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH cte AS
+(
+SELECT *
+, DENSE_RANK() OVER(ORDER BY salary DESC) AS rn
+FROM employee
+)
+
+SELECT IFNULL(
+(SELECT salary 
+FROM cte
+WHERE rn = 2 
+LIMIT 1), NULL) AS secondhighestsalary ;
