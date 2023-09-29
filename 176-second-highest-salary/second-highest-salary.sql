7ff53707670e495a5a02c86c0b42de32e99ef317
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-09-29 16:22:43 +0530

    Time: 398 ms (96.84%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -5,9 +5,16 @@ SELECT *
 , DENSE_RANK() OVER(ORDER BY salary DESC) AS rn
 FROM employee
 )
-
+/*
 SELECT IFNULL(
 (SELECT salary 
 FROM cte
 WHERE rn = 2 
 LIMIT 1), NULL) AS secondhighestsalary ;
+*/
+
+SELECT COALESCE(
+(SELECT salary  
+FROM cte
+WHERE rn = 2 
+LIMIT 1),NULL) AS SecondHighestSalary
