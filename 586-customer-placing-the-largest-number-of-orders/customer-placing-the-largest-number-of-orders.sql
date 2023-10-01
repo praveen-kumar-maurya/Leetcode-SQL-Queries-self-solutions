dc85d9a2bab0fa8ab045669e37764d05b8bb1f0d
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-01 15:15:36 +0530

    Time: 810 ms (97.02%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -3,3 +3,17 @@ SELECT customer_number
 FROM orders
 GROUP BY customer_number
 HAVING COUNT(order_number) = (SELECT MAX(x.count) FROM (SELECT COUNT(order_number) AS count FROM orders GROUP BY customer_number) AS x)
+
+
+/* Alternate solution but a bit doubtful 
+WITH CTE AS
+(
+SELECT *
+,COUNT(order_number) OVER(PARTITION BY customer_number) AS count
+FROM Orders 
+)
+
+SELECT DISTINCT(customer_number)
+FROM CTE 
+WHERE count = (SELECT MAX(count) FROM CTE )
+*/
