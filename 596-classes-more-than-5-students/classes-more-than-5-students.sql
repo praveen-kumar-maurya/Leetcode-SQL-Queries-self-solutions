dbcb52ada766d4ca162997ec7a591f9a8416818e
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-01 15:21:16 +0530

    Time: 633 ms (58.20%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT class
 FROM
 (SELECT class, COUNT(*) AS count
@@ -6,3 +7,10 @@ FROM courses
 GROUP BY class
 ) AS x
 WHERE x.count >= 5
+*/
+
+/* Better approach */
+SELECT class
+FROM courses
+GROUP BY class
+HAVING COUNT(*) >= 5;
