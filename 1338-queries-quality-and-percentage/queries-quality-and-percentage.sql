eb3b947099e7fd3cd2a97a0922980674b842137e
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-14 14:57:18 +0530

    Time: 538 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -9,6 +9,9 @@ GROUP BY q.query_name;
 */
 
 # Simpler way
+
 SELECT query_name, ROUND(AVG(rating/position),2) AS quality, COALESCE(ROUND(SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END)*100/COUNT(*) ,2),0) AS poor_query_percentage
 FROM queries
 GROUP BY query_name;
+
+
