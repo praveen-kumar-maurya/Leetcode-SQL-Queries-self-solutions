5403a9ae7c82ef4f85695521d23dcc03c31069a0
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-14 16:35:11 +0530

    Time: 2315 ms (83.03%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,4 +1,34 @@
 # Write your MySQL query statement below
+
+# Solution (Better)
+
+(
+SELECT u.name AS results
+FROM users u
+LEFT JOIN movierating mr
+ON u.user_id = mr.user_id
+GROUP BY u.name
+ORDER BY COUNT(mr.rating) DESC, u.name ASC
+LIMIT 1
+)
+
+UNION ALL
+
+(
+SELECT m.title AS results
+FROM movies m
+LEFT JOIN movierating mr
+ON m.movie_id = mr.movie_id
+WHERE YEAR(mr.created_at) = 2020 AND MONTH(mr.created_at) = 2
+GROUP BY m.title
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1
+)
+
+
+# Alternate solution
+
+/*
 WITH cte AS
 (
 SELECT mr.*,u.name,m.title
@@ -27,3 +57,5 @@ GROUP BY title
 ORDER BY AVG(rating) DESC, title
 LIMIT 1
 );
+*/
+
