6158057ceadbb4e4e78fdd48b983e6732dcba6cd
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-20 16:38:57 +0530

    Time: 1129 ms (82.79%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
+# Better solution
+/*
 SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM logins
 WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id
+*/
+
+# Alternate method
+SELECT DISTINCT user_id,
+FIRST_VALUE(time_stamp) OVER(PARTITION BY user_id ORDER BY time_stamp DESC) AS last_stamp
+FROM logins
+WHERE YEAR(time_stamp) = 2020
