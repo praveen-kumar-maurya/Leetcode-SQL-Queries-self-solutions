8b5574fdcb29592d18ea311e0f87820d629f7fed
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-23 14:23:31 +0530

    Time: 1000 ms (85.03%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,5 +1,30 @@
 # Write your MySQL query statement below
+
+WITH cte AS
+(
+SELECT e.employee_id AS employee_id
+FROM employees e
+INNER JOIN salaries s
+ON e.employee_id = s.employee_id
+)
+
+SELECT e.employee_id AS employee_id
+FROM employees e
+LEFT JOIN cte c
+ON e.employee_id = c.employee_id
+WHERE c.employee_id IS NULL
+
+UNION 
+
+SELECT s.employee_id AS employee_id
+FROM salaries s
+LEFT JOIN cte c
+ON s.employee_id = c.employee_id
+WHERE c.employee_id IS NULL
+ORDER BY employee_id
+
 # Approach -1 (Using union)
+/*
 SELECT employee_id
 FROM employees
 WHERE employee_id NOT IN 
@@ -22,4 +47,4 @@ INNER JOIN salaries s
 ON e.employee_id = s.employee_id
 )
 ORDER BY employee_id;
-
+*/
