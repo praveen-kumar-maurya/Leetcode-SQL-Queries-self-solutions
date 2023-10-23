52bdb890b41e26a173c4e47ca3629c860acce2cf
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-10-23 14:29:52 +0530

    Time: 564 ms (91.06%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -8,5 +8,5 @@ WHERE salary < 30000
 
 SELECT employee_id
 FROM cte 
-WHERE manager_id IS NOT NULL AND manager_id  NOT IN (SELECT DISTINCT employee_id FROM employees)
+WHERE manager_id IS NOT NULL AND manager_id  NOT IN (SELECT employee_id FROM employees)
 ORDER BY employee_id;
