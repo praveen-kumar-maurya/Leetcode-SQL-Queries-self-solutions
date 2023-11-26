e62a430d63cc6773c13e852a4a83e9b596fe8889
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-11-26 14:12:02 +0530

    Time: 561 ms (22.35%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -5,16 +5,18 @@ SELECT *
 , DENSE_RANK() OVER(ORDER BY salary DESC) AS rn
 FROM employee
 )
-/*
+
 SELECT IFNULL(
 (SELECT salary 
 FROM cte
 WHERE rn = 2 
 LIMIT 1), NULL) AS secondhighestsalary ;
-*/
+
 /* Even COALESCE can also be used instead of IFNULL*/
+/*
 SELECT COALESCE(
 (SELECT salary  
 FROM cte
 WHERE rn = 2 
-LIMIT 1),NULL) AS SecondHighestSalary
+LIMIT 1),NULL) AS SecondHighestSalary;
+*/
