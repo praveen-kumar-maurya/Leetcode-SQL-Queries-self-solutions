f0d1fb708179e6d7a83c29d95389a7a6f6226edd
PRAVEEN KUMAR MAURYA <88583491+praveen-kumar-maurya@users.noreply.github.com>
2023-11-26 14:35:21 +0530

    Time: 392 ms (96.34%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+SELECT 
+    (SELECT DISTINCT salary 
+     FROM Employee 
+     ORDER BY salary DESC 
+     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+/*
 WITH cte AS
 (
 SELECT *
@@ -10,7 +17,8 @@ SELECT IFNULL(
 (SELECT salary 
 FROM cte
 WHERE rn = 2 
-LIMIT 1), NULL) AS secondhighestsalary ;
+LIMIT 1), NULL) AS SecondHighestSalary ;
+*/
 
 /* Even COALESCE can also be used instead of IFNULL*/
 /*
@@ -18,5 +26,5 @@ SELECT COALESCE(
 (SELECT salary  
 FROM cte
 WHERE rn = 2 
-LIMIT 1),NULL) AS SecondHighestSalary;
+LIMIT 1), NULL) AS SecondHighestSalary;
 */
